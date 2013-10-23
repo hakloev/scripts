d5e24b0c2b0f46e4c2b7ca833bbb138a5c9d4c01
Håkon Ødegård Løvdal <hakloev@gmail.com>
2013-10-23 12:33:37 +0200

    Updated script for command line input

--- applestore.py
+++ applestore.py
@@ -2,6 +2,7 @@ import urllib2
 import hashlib
 import os
 import time
+import sys
 from BeautifulSoup import BeautifulSoup
 
 def getResponseAndMd5():
@@ -18,15 +19,25 @@ def getResponseAndMd5():
                 return str(hashlib.md5(str(tag)).hexdigest())
 
 if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        interval = int(sys.argv[1])
+    else:
+        interval = int(raw_input('Hvor langt oppdateringsintervall vil du ha i hele minutter? '))
+    
     Flag = True
     while Flag:
         md5 = getResponseAndMd5()
         if md5 == '43d30f94f0fcde617a4eb441b37851fe':
             print 'Ikke lansert enda!'
         else:
+            os.system('say Something has changed on Apples webpage!')
+            os.system('open -a Safari http://store.apple.com/no/buy-iphone/iphone5s')
+            print 'Lansert, eller noe er i det minste endret!'
             print 'Den nye md5-summen: ', md5
-            print 'LANSERT'
             Flag = False
-        print 'Sover i 10 sekunder'
-        time.sleep(10)
-
+        
+        if interval <= 1:
+            print 'Sover i', interval, 'minutt'
+        else:
+            print 'Sover i', interval, 'minutter'
+        time.sleep(interval * 60)
