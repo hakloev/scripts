d1f455f0a5dbac2515bfb5da20f8e48cfb8a0904
Håkon Ødegård Løvdal <hakloev@gmail.com>
2013-11-15 09:26:38 +0100

    made universal

--- sshkeys.py
+++ sshkeys.py
@@ -1,4 +1,5 @@
 import os
+import getpass
 import sys
 
 if len(sys.argv) <= 2:
@@ -6,7 +7,7 @@ if len(sys.argv) <= 2:
     print 'Usage: [port] is voluntary'
     sys.exit()
 
-path = "/Users/hakloev/.ssh/id_rsa.pub"
+path = str(os.getenv("HOME")) + "/.ssh/id_rsa.pub"
 user = str(sys.argv[1])
 host = str(sys.argv[2])
 
@@ -17,14 +18,18 @@ else:
 
 def getPublicKey():
     if os.path.exists(path):
-        print "id_rsa exists"
+        print "id_rsa.pub exists"
+        print path
     else:
-        print "id_rsa doesn't exist, creating now!"
+        print "id_rsa.pub doesn't exist, creating now!"
         os.system('ssh-keygen')
+        print path
 
 def postKey():
+    print 'deploying key to ', user, '@', host 
     os.system('cat ' + path + ' | ssh -p ' + port + ' '+  user + '@' + \
     host + " 'mkdir -p .ssh; cat >> .ssh/authorized_keys'")
-    
+    print 'key deployed'
+
 getPublicKey()
 postKey()
