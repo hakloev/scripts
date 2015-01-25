a206fcdf6c87916cd570f93c74f78de9c0952402
Håkon Ødegård Løvdal <hakloev@gmail.com>
2015-01-25 14:04:33 +0100

    added script for ss from @kradalby

---
+++ ss.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# This script is written by kradalby (username: kradalby here at GitHub)
+
+#Use this as a osx service.
+# Edit for own needs.
+# 
+# Install:
+# Open Automater.app
+# Create Service
+# Change to no input and add Run Shell Script
+# Paste path to script ( make sure it is executable )
+# Save and add a shortcut to it under System Preferences > Keyboard > Shortcuts
+
+url=https://hakloev.no/sc/
+filename=`date '+%Y%m%d%H%M%S'`.png
+path=~/Pictures/Screenshots/
+mkdir -p $path
+screencapture -o -i $path$filename
+scp $path$filename hakloev@hakloev.no:/var/www/hakloev.no/screenshots/ > /dev/null
+printf $url$filename | pbcopy
